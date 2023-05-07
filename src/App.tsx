961be48801406a556d2d0d30e53376089ff66085
shlande <shlandeme@gmail.com>
2023-05-07 11:26:57 +0800

    完成card动态数据

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Temperature, {Prop as TemperaturePros} from "./Temperature";
-import Card from "./Card";
+import Card, {Data} from "./Card";
 import React from "react";
 import "./App.css"
 
@@ -18,15 +18,22 @@ const data:TemperaturePros[]=  [
   },
   {
     humidity: 19, temperature: 60, time: new Date(),
-  }
-]
+  }]
+
+const current:Data = {
+  detail: [{
+    title: '2级',
+    value: "北风"
+  }],
+  advice: "今天白天晴，夜晚多云，比昨天热一些，现在23°，空气不错。",
+  condition: "AQI 优",
+  country: "中国", location: "北京市", province: "北京", region: "北京市", temperature: "23", weather: "晴"
+}
 
 function App() {
-
-
   return (
     <div className={"container"}>
-      <div className={"card-container"}><Card/></div>
+      <div className={"card-container"}><Card data={current}/></div>
       <div className={"temperature"}>
         <div className={"temperature-title title"}><span>7日温度湿度</span></div>
         <Temperature data={data}/>
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,42 +1,77 @@
+import { ReactElement} from "react"
 import "./Card.css"
 
-export default () => {
+export type Data = {
+  location: string,
+  country: string,
+  province: string
+  region: string
+  condition: string
+  temperature: string
+  weather: "阴" | "晴" | "雨" | "雪"
+  advice: string
+  detail: {
+    title: string
+    value: string
+  }[]
+}
+
+const formatDate = (date: Date) => {
+  return `${date.getFullYear()}-${formatInt(date.getMonth() + 1)}-${formatInt(date.getDate())} ${formatInt(date.getHours())}:${formatInt(date.getMinutes())}`
+}
+
+const formatInt = (num: number): string => {
+  return ("0" + num).slice(-2)
+}
+
+const generateWeatherImg = (weather: Data["weather"]) => {
+  switch (weather) {
+    case "晴":
+      return "/100.png"
+    case "阴":
+      return "/101.png"
+    case "雨":
+      return "/305.png"
+    case "雪":
+      return "/101.png"
+  }
+}
+
+export default (props: { data: Data }) => {
+  const {region, province, country, condition, temperature, weather, advice, detail} = props.data
+
+  const Detail: ReactElement[] = []
+  detail && detail.forEach((item,index) => {
+    Detail.push(<div key={index} className={"current-basic-item"}>
+      <p className={"title"}>{item.title}</p>
+      <p className={"content"}>{item.value}</p>
+    </div>)
+  })
   return <div className="current-weather">
     <div className="location">
       <div>
-        <h1 className={"content"}>北京市</h1>
-        <span className={"mini"}>北京市/中国</span>
+        <h1 className={"content"}>{region}</h1>
+        <span className={"mini"}>{province}/{country}</span>
       </div>
-      <p className="mini">2023-05-07 09:24</p>
+      <p className="mini">{formatDate(new Date())}</p>
     </div>
     <div className="weather">
       <div className={"condition-container"}>
-        <div className={"condition"}>AQI 优</div>
+        <div className={"condition"}>{condition}</div>
       </div>
       <div className="weather-data">
-        <img src="/100.png" alt="QWeather"/>
+        <img src={generateWeatherImg(weather)} alt="Weather"/>
         <div className={"weather-data-detail"}>
-          <span className={"display"}>21°</span>
-          <span>晴</span>
+          <span className={"display"}>{temperature}°</span>
+          <span>{weather}</span>
         </div>
       </div>
     </div>
     <div className="current-abstract content">
-        今天白天晴，夜晚多云，比昨天热一些，现在21°，有风，空气不错。
+      {advice}
     </div>
     <div className="current-basic">
-       <div className={"current-basic-item"}>
-         <p className={"title"}>3级</p>
-         <p className={"content"}>东北风</p>
-       </div>
-      <div className={"current-basic-item"}>
-        <p>21%</p>
-        <p>相对湿度</p>
-      </div>
-      <div className={"current-basic-item"}>
-        <p>强</p>
-        <p>紫外线</p>
-      </div>
+      {Detail}
     </div>
   </div>
 }
